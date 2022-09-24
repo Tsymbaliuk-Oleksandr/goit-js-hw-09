6c28d43a36520581942e4009c4ac0634868786ec
Tsymbaliuk-Oleksandr <103380042+Tsymbaliuk-Oleksandr@users.noreply.github.com>
2022-09-24 14:41:05 +0000

    Deploying to gh-pages from @ Tsymbaliuk-Oleksandr/goit-js-hw-09@66e3a33f7f68165884115ab9e8282b2bbd8ab1ad 🚀

---
+++ 01-color-switcher.94197783.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;function n(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.disabled=!0,t.addEventListener("click",(function(){d=setInterval(n,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.94197783.js.map
